c12b74da91b6550ecc97b74d8f6ae327270e1197
nroberts1011 <134349810+nroberts1011@users.noreply.github.com>
2023-12-11 00:35:57 -0800

    Add files via upload

--- Mapping.cpp
+++ Mapping.cpp
@@ -1,10 +1,50 @@
 #include "Mapping.h"
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+using namespace std;
 
-
-Mapping::Mapping(){
-  
+Mapping::Mapping() {
+  // initialize map to empty
 }
 
-void Mapping::readFromFile(){
-  
+void Mapping::readFromFile(string filename) {
+  string line;
+  ifstream file;
+  file.open(filename);
+  while (getline(file, line)) {
+
+    string title;
+    title = line.substr(7);
+
+    getline(file, line); // gets genres line
+    string g;
+    g = line.substr(7);
+
+    getline(file, line);
+    string description; // unused?
+    description = line.substr(13);
+
+    // add each genre into genres set
+    set<string> genres;
+    string curGenre;
+
+    for (int i = 0; i < g.length(); i++) {
+      // adds each genre from the line to the genres set
+      if (g[i] == ',') {
+        curGenre = g.substr(0, i);
+        genres.insert(curGenre);
+        g = g.substr(i + 2);
+        i = 0;
+      } else if (i == g.length() - 1) {
+        curGenre = g;
+        genres.insert(curGenre);
+      }
+    }
+
+    // sets the map pair at the key of the title to the value of the genres
+    movies[title] = genres;
+  }
 }
--- Mapping.h
+++ Mapping.h
@@ -2,16 +2,21 @@
 #define _MAPPING_H_
 
 #include <string>
+#include <map>
+#include <set>
+#include <iostream>
+#include <fstream>
 #include "ListNode.h"
+using namespace std;
 
 class Mapping{
 private:
-
+  map<string, set<string>> movies;
 
 public:
   Mapping();
   
-  void readFromFile(); //this function does what Mapping() should do
+  void readFromFile(string filename); 
 };
 
 #endif
